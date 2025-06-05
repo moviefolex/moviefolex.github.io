47ed778e8a55e0e8426f19f838ac82cb76b97802
movie folex <freelancingwebsite41@gmail.com>
2025-06-05 12:31:59 +0530

    Update movie.js

--- movie.js
+++ movie.js
@@ -2,14 +2,15 @@ const movies = [
   {
     id: 1,
     title: "The Royals",
-    image: "https://m.media-amazon.com/images/M/MV5BMWE3ZTY3NDkt...jpg",
-    genre: "Drama",
+    image: "https://i.pinimg.com/736x/2f/6c/74/2f6c745376fe03d434cad1c58e7c67f9.jpg",
+    genre: "ROMANCE, DRAMA, COMEDY",
     language: "Hindi",
-    resolution: "1080p",
+    resolution: "1080p, 720p, 480p",
     review: "A royal family’s secrets exposed through suspenseful storytelling.",
     downloadLinks: [
       { label: "480p", url: "https://example.com/royals-480p" },
-      { label: "720p", url: "https://example.com/royals-720p" }
+      { label: "720p", url: "https://example.com/royals-720p" },
+      { label: "1080p", url: "https://gplinks.co/0MR82q" }
     ]
   },
   {
